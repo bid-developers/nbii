5f5deb75c6003e385d0a801c6cf0c86db06436a2
leslie <lmavonyani@gmail.com>
2017-08-28 17:43:54 +0100

    Leslie

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+
+import { FirebaseListObservable,
+    FirebaseObjectObservable,
+    AngularFireDatabase
+} from 'angularfire2/database';
 
 @Component({
-  selector: 'page-events',
-  templateUrl: 'events.html'
+    selector: 'page-events',
+    templateUrl: 'events.html'
 })
+
 export class EventsPage {
+    items: FirebaseListObservable<any[]>;
+    item: FirebaseObjectObservable<any>;
+    constructor(db: AngularFireDatabase/*, public navCtrl: NavController*/) {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+        this.items =  db.list('/event',{
+            query: {
+                orderByChild: 'score'
+            }
+        })
+        //       console.log(this.items)
+    }
+    /*    return():void {
+     this.navCtrl.setRoot(HomePage);
+     }*/
 }
