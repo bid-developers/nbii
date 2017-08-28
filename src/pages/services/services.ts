8889b561f43b0f4d60559c59b907530dab61b511
leslie <lmavonyani@gmail.com>
2017-08-28 18:09:35 +0100

    Leslie services folder created

---
+++ src/pages/services/services.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+
+import { FirebaseListObservable,
+    FirebaseObjectObservable,
+    AngularFireDatabase
+} from 'angularfire2/database';
+
+@Component({
+    selector: 'page-services',
+    templateUrl: 'services.html'
+})
+
+export class ServicesPage {
+    items: FirebaseListObservable<any[]>;
+    item: FirebaseObjectObservable<any>;
+    constructor(db: AngularFireDatabase/*, public navCtrl: NavController*/) {
+
+        this.items =  db.list('/service',{
+            query: {
+                orderByChild: 'score'
+            }
+        })
+        //       console.log(this.items)
+    }
+    /*    return():void {
+     this.navCtrl.setRoot(HomePage);
+     }*/
+}
